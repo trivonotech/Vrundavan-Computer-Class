90932e7f37978100e7b3f57fa4bb73b88684ad5d
yash <yash.chaniyara@icloud.com>
2025-12-14 18:01:52 +0530

    refactor: Rename client and server build output directories and update package.json references.

--- vite.config.server.ts
+++ vite.config.server.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       fileName: "production",
       formats: ["es"],
     },
-    outDir: "dist/server",
+    outDir: "dist-server",
     target: "node22",
     ssr: true,
     rollupOptions: {
